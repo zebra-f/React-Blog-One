0b27c763e7b4e0c93c763aff86c29d05457f72ff
zebra-f <rafal.zebrowski@gmail.com>
2022-08-28 21:29:33 +0200

    initialized React Blog App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Routes, Route, Link } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Posts from "./components/Posts";
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Posts />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Posts.js
@@ -0,0 +1,12 @@
+const Posts = () => {
+  return (
+    <>
+      <ul>
+        <li>Post 1</li>
+        <li>Post 2</li>
+      </ul>
+    </>
+  );
+};
+
+export default Posts;
