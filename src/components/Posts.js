835a1baaec8878cf9e1c9de4f467be6d9e4893e3
zebra-f <rafal.zebrowski@gmail.com>
2022-09-01 20:24:31 +0200

    servers not working properly catch

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,20 +4,29 @@ import Post from "./Post";
 
 function Posts() {
   const [posts, setPosts] = useState(null);
-
+  const [postsApiError, setPostsApiError] = useState(false);
   useEffect(() => {
     const apiUrl = "http://127.0.0.1:8000/api/";
     fetch(apiUrl)
       .then((response) => response.json())
       .then((posts) => {
         setPosts(posts);
+      })
+      .catch((error) => {
+        setPostsApiError(true);
       });
   }, []);
 
-  if (!posts) {
+  if (!posts && !postsApiError) {
     return (
       <div className="PostsLoading">
         <p>Loading...</p>
+      </div>
+    );
+  } else if (postsApiError) {
+    return (
+      <div className="PostsLoading">
+        <p>We have a problem with our servers, come back later. Sorry.</p>
       </div>
     );
   }
