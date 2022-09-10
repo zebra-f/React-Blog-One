7d6cec1db7ef8f694342d2445abdaf7ff66acb52
zebra-f <rafal.zebrowski@gmail.com>
2022-09-10 23:15:03 +0200

    login added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,85 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import axiosInstance from "../api/axios.js";
+
 function Login() {
+  const navigate = useNavigate();
+
+  const initialFormData = Object.freeze({
+    email: "",
+    password: "",
+  });
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [submitError, setSubmitError] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    axiosInstance
+      .post(`token/`, {
+        email: formData.email,
+        password: formData.password,
+      })
+      .then((response) => {
+        localStorage.setItem("access_token", response.data.access);
+        localStorage.setItem("refresh_token", response.data.refresh);
+        axiosInstance.defaults.headers["Authorization"] =
+          "JWT " + localStorage.getItem("access_token");
+
+        console.log(response);
+        console.log(response.data);
+        navigate("/");
+      })
+      .catch((error) => {
+        setSubmitError(true);
+        console.log(error);
+      });
+  };
+
   return (
     <div className="Login">
-      <h2>Login</h2>
+      {/* Login Form */}
+      <br />
+      {submitError && <h2>Wrong credentials, try again.</h2>}
+      <form className="login-form" action="">
+        <div className="email">
+          <label className="form__label">Email: </label>
+          <input
+            className="form__input"
+            name="email"
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Your Email"
+          />
+        </div>
+        <div className="password">
+          <label className="form__label">Password: </label>
+          <input
+            className="form__input"
+            name="password"
+            type="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Your Password"
+          />
+        </div>
+        <div className="login-submit-button">
+          <button onClick={handleSubmit} type="submit">
+            Login
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
